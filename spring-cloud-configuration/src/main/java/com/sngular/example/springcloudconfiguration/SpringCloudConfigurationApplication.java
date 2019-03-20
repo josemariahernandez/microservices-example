1ec8d98df2ccf1b1c680624c6cb6e0bee3f5d37d
Chema <jose.hernandez@fon.com>
2019-03-20 10:04:03 +0100

    First commit

---
+++ spring-cloud-configuration/src/main/java/com/sngular/example/springcloudconfiguration/SpringCloudConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.sngular.example.springcloudconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigurationApplication.class, args);
+	}
+
+}
